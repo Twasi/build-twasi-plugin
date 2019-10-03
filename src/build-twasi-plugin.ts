eb8b580728faf4d14eb56873c0d50b703e685602
Lars Bärtschi <lars.baertschi@bluewin.ch>
2019-10-03 14:07:59 +0200

    initial commit

---
+++ src/build-twasi-plugin.ts
@@ -0,0 +1,16 @@
+import * as core from '@actions/core';
+import build from './build';
+
+async function run() {
+  try {
+    let pluginName = core.getInput('pluginName', { required: true });
+
+    console.log(`Building plugin ${pluginName}`);
+
+    await build.runBuild(pluginName);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
