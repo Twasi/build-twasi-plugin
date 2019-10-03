f44df930bbd5754f71f2b1fe74361a1fd128190a
Lars Bärtschi <lars.baertschi@bluewin.ch>
2019-10-03 16:45:18 +0200

    Read data from file

--- src/build-twasi-plugin.ts
+++ src/build-twasi-plugin.ts
@@ -1,13 +1,15 @@
 import * as core from '@actions/core';
+import * as fs from 'fs';
+import * as yaml from 'yaml';
 import build from './build';
 
 async function run() {
   try {
-    let pluginName = core.getInput('plugin-name', {required: true});
+    const pluginYmlRaw = fs.readFileSync('./src/main/resources/plugin.yml', 'utf8');
+    const pluginYml = yaml.parse(pluginYmlRaw);
+    console.log(`Building plugin ${pluginYml['name']}`);
 
-    console.log(`Building plugin ${pluginName}`);
-
-    await build.runBuild(pluginName);
+    await build.runBuild(pluginYml['name']);
   } catch (error) {
     core.setFailed(error.message);
   }
