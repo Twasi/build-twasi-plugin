5e2274a04718ad37663f32f3223f683598686114
Lars Bärtschi <lars.baertschi@bluewin.ch>
2019-10-04 07:57:17 +0200

    correct maven repo format

--- src/build.ts
+++ src/build.ts
@@ -1,9 +1,9 @@
 import * as exec from '@actions/exec';
 
 async function runBuild(pluginName: string, version: string) {
-  await exec.exec(`mvn versions:set -DnewVersion=${version}`);
+  await exec.exec(`mvn -q -B versions:set -DnewVersion=${version}`);
   await exec.exec(
-    'mvn -q -B clean compile package deploy -DaltDeploymentRepository=https://maven.pkg.github.com/Twasi -Dtoken=$GH_TOKEN'
+    'mvn -q -B clean compile package deploy -DaltDeploymentRepository=snapshots::default::https://maven.pkg.github.com/Twasi -Dtoken=$GH_TOKEN'
   );
 }
 
