19b0594530c62f959c29b036892ee33488bf4c87
Lars Bärtschi <lars.baertschi@bluewin.ch>
2019-10-04 08:21:52 +0200

    try to find path

--- src/build.ts
+++ src/build.ts
@@ -3,6 +3,9 @@ import * as exec from '@actions/exec';
 async function runBuild(pluginName: string, version: string) {
   const mvn = `mvn - q -B -s ./settings.xml`;
 
+  await exec.exec('ls -la .');
+  await exec.exec('ls -la ..');
+
   await exec.exec(`${mvn} versions:set -DnewVersion=${version}`);
   await exec.exec(
     '${mvn} clean compile package deploy -DaltDeploymentRepository=snapshots::default::https://maven.pkg.github.com/Twasi -Dserver.password=$GH_TOKEN -Dserver.username=LarsBaertschi'
